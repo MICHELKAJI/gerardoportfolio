0046b7402f60f190a1c1841196c85ff64d4c255b
Michel <fistonkajibwami2@gmail.com>
2025-06-02 03:42:05 -0330

    add new version

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,24 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ['./src/**/*.{html,ts}'],
+  /** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {
-      animation: {
-        'fade-in': 'fadeIn 1s ease-out forwards',
-        'slide-right': 'slideRight 1s ease-out forwards',
-        'ping-slow': 'ping 3s cubic-bezier(0, 0, 0.2, 1) infinite',
-      },
-      keyframes: {
-        fadeIn: {
-          '0%': { opacity: '0' },
-          '100%': { opacity: '1' },
-        },
-        slideRight: {
-          '0%': { width: '0%' },
-          '100%': { width: '100%' },
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
